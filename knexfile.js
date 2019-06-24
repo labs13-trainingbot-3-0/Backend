d5128c8ad369701276b9d2f3f8d3990aed98bb01
Thomas Folbrecht <tfolbrecht@gmail.com>
2019-06-24 16:28:49 -0400

    add sqlite knex

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,19 @@
 require('dotenv').config()
 
 module.exports = {
+  devsqlite: {
+    client: "sqlite3",
+    connection: {
+      filename: "./models/dev.sqlite3"
+    },
+    migrations: {
+      directory: "./models/migrations/" // knex migrate:latest --env=devsqlite
+    },
+    seeds: {
+      directory: "./models/liteseeds/" //  knex seed:run --env=devsqlite
+    },
+    useNullAsDefault: true
+  },
   development: {
     client: 'pg',
     connection: 'postgres://localhost/tb3', // postgres DB name is tb3,
